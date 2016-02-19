e8a7c564d6b5ce2993e72bfda55a7bcd3e2f766f
Julio Colomer <juliocolomer@julios-air.att.net>
2016-02-18 17:28:46 -0800

    networking services section progressing

--- app/networkingServices/httpService/httpService.js
+++ app/networkingServices/httpService/httpService.js
@@ -3,7 +3,55 @@
     "use strict";
 
     angular.module('httpService', [])
-        .controller('ControllerHttp', ['$scope', function ($scope) {
+
+        .factory('book', ['$http', function ($http) {
+
+            return {
+                getBook: getBook
+            };
+
+
+            function getBook () {
+
+                return $http({
+                    method: 'GET',
+                    url: 'content/book.json'
+                })
+                    .then(getResponse)
+                    .catch(getError);
+
+            }
+
+            function getResponse (response) {
+                return response.data;
+            }
+
+            function getError (error) {
+                return $q.reject ("Error: " + response.status);
+            }
+
+
+
+
+
+
+
+        }])
+
+        .controller('ControllerHttp', ['$scope', 'book', function ($scope, book) {
+
+            book.getBook ()
+                .then (getBookSuccess)
+                .catch (getBookError);
+
+            function getBookSuccess (theBook) {
+                $scope.libro = theBook.title;
+            }
+
+            function getBookError (error) {
+                console.log(error);
+            }
+
 
         }])
 
